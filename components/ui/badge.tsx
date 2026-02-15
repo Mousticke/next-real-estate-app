94ebdab24f731e602644874e6b939fce13b1078a
Akim Benchiha <akim.benchiha@gmail.com>
2026-02-15 23:29:11 +0100

    feat: custom shadcn wip to remove radix import package

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import { Slot } from "radix-ui";
+import type * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -34,7 +34,7 @@ const badgeVariants = cva(
 );
 
 function Badge({ className, variant = "default", asChild = false, ...props }: React.ComponentProps<"span"> & VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot.Root : "span";
+  const Comp = asChild ? Slot : "span";
 
   return <Comp data-slot="badge" data-variant={variant} className={cn(badgeVariants({ variant }), className)} {...props} />;
 }
